4eca4b3e58f42d17548a0041890b4ae53872c3fc
Justin <justin.strayhorn@hotmai.com>
2017-01-17 16:18:18 -0600

    starting with old project

---
+++ js/footer.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from 'react-router'
+
+
+export default React.createClass({
+
+  render () {
+
+    return (
+      <section>
+        <footer>
+          <h1>Test</h1>
+        </footer>
+      </section>
+    )
+  }
+})
---
+++ js/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+
+export default React.createClass({
+  render(){
+
+    return(
+      <section>
+        <h1> Justin's Portfolio </h1>
+      </section>
+    )
+    }
+  })
