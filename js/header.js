545ae6a07cc69d7f8057145710905967e68db8ed
Justin <justin.strayhorn@hotmai.com>
2017-01-18 13:27:47 -0600

    modified styling, added links to pages

--- js/header.js
+++ js/header.js
@@ -7,6 +7,11 @@ export default React.createClass({
     return (
       <header className="header">
         <h1 className="headerText"> Portfolio </h1>
+          <nav>
+            <Link to="/projects" className="navLink"> View Projects </Link>
+            <Link to="/" className="navLink"> Home </Link>
+            <Link to="/blog" className="navLink"> Blog </Link>
+          </nav>
       </header>
     )
   }
