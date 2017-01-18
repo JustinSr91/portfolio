4162498eb185274ac71a04a50ea273e1c7d6e95e
Justin <justin.strayhorn@hotmai.com>
2017-01-17 20:20:51 -0600

    added styling to home page, added blog content to blog page

--- js/footer.js
+++ js/footer.js
@@ -9,7 +9,7 @@ export default React.createClass({
     return (
       <section>
         <footer>
-          <h1> The End </h1>
+          <h1> ########################### </h1>
         </footer>
       </section>
     )
--- js/home.js
+++ js/home.js
@@ -9,12 +9,12 @@ export default React.createClass({
       <section>
         <h1> Quick Info </h1>
           <img src="photo.jpg"></img>
-          <li> Justin Strayhorn Sr.</li>
-          <li> From San Antonio, Texas </li>
-          <a href="https://github.com/JustinSr91">
-            <li> Link to My GitHub </li>
+          <li className="infoList"> Justin Strayhorn Sr.</li>
+          <li className="infoList"> From San Antonio, Texas </li>
+          <a href="https://github.com/JustinSr91" className="link" target="_blank">
+            <li className="infoList"> Link to My GitHub </li>
           </a>
-          @// TODO: Add bio paragraph
+          <p> My name is Justin and I am from Cincinnati, OH. I have lived in San Antonio for the last 5 years and have been in various roles with various companies. I have a background in Insurance, Oil and Gas, and Military, but I recently have decided to change careers (Software Development) where I know I can excel. I have a wife and 4 children who are my motivation to be the best version of myself that I can. I am currently hoping to obtain a Junior Developer position which will give me the experience I need to become highly skilled.</p>
       </section>
     )
     }
